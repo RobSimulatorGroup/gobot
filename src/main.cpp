0336fc544d837f6b1d0ce735d7f641291dc7261d
Qiqi Wu <1258552199@qq.com>
2022-12-03 20:29:06 +0800

    update lisense

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/* The gobot is a robot simulation platform.
+ * Copyright(c) 2021-2022, RobSimulatorGroup, Qiqi Wu<1258552199@qq.com>.
+ * Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+ * This version of the GNU Lesser General Public License incorporates the terms and conditions of version 3 of the GNU General Public License.
+ * This file is created by Qiqi Wu, 22-11-26
+*/
+
 
 #include "gobot/editor/main_window.hpp"
 #include <QApplication>
