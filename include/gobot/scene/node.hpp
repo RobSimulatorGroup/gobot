71e453f2b065bf795867f118750d2fa8583f4e24
Zikun Yu <stephenyu113@outlook.com>
2023-01-21 19:45:36 +0800

    Add NodePath (#25)
    * 23 add nodepath 1 (#24)
    
    * update
    Co-authored-by: Qiqi Wu <1258552199@qq.com>
    
    * updated node path
    
    * updated node_path
    
    * implemented class NodePath
    
    * added unittest for class NodePath
    
    * updated err_macros
    
    * added docstring
    
    * added registration
    
    * updated test
    
    * updated NodePath class
    
    * Test type of  NodePath  (#29)
    
    Updated implementations and tests for NodePath registration.
    
    * renamed GetStrData
    
    * added node_path.cpp
    
    Co-authored-by: Qiqi Wu <37368540+Wuqiqi123@users.noreply.github.com>

--- include/gobot/scene/node.hpp
+++ include/gobot/scene/node.hpp
@@ -8,19 +8,105 @@
 #pragma once
 
 #include "gobot/core/object.hpp"
+#include "gobot/error_marcos.hpp"
 
 namespace gobot {
 
+class NodePath;
+class SceneTree;
+
 class Node : public Object {
     GOBCLASS(Node, Object)
-
 public:
-
     Node();
 
-private:
-    std::vector<Node*> children_node_;
+    ~Node() = default;
 
+    Node(const String& name);
+
+    String GetName() const;
+
+    void SetName(const String & name);
+
+    void AddChild(Node *child);
+
+    void AddSibling(Node *sibling);
+
+    void RemoveChild(Node *child);
+
+    Node* GetChild(int p_index) const;
+
+    bool HasNode(const NodePath& path) const;
+
+    // https://docs.godotengine.org/zh_CN/stable/tutorials/scripting/scene_unique_nodes.html
+    Node* GetNode(const NodePath &path) const;
+
+    Node* GetNodeOrNull(const NodePath &path) const;
+
+    Node* GetParent() const;
+
+    void PrintTree();
+
+    void PrintTreePretty();
+
+    FORCE_INLINE SceneTree* GetTree() const {
+        ERR_FAIL_COND_V(!tree_, nullptr);
+        return tree_;
+    }
+
+    FORCE_INLINE bool IsInsideTree() const { return inside_tree_; }
+
+    bool IsAncestorOf(const Node *node) const;
+
+    NodePath GetPath() const;
+
+    NodePath GetPathTo(const Node *node) const;
+
+    Node* FindCommonParentWith(const Node* node) const;
+
+protected:
+    void AddChildNoCheck(Node *child, const String& name);
+
+    void SetNameNoCheck(const String& name);
+
+    void SetOwnerNoCheck(Node *owner);
+
+    void Notification(NotificationType notification);
+
+private:
+    friend class SceneTree;
+
+    void PropagateReverseNotification(int p_notification);
+
+    void PropagateEnterTree();
+
+    void PropagateReady();
+
+    void PropagateExitTree();
+
+    void PropagateAfterExitTree();
+
+    friend class SceneTree;
+
+    void SetTree(SceneTree* tree);
+
+    Node *GetChildByName(const String& name) const;
+
+    void ValidateChildName(Node* child);
+
+    void GenerateSerialChildName(const Node* child, String&name) const;
+
+private:
+    String name_;
+    std::vector<Node*> children_node_;
+    mutable NodePath *path_cache_ = nullptr;
+
+    bool inside_tree_ = false;
+    SceneTree* tree_ = nullptr;
+
+    Node* parent_ = nullptr;
+
+    // TODO(wqq): Do we need ower?
 };
 
 }
