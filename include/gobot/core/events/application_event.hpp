905260a939732cc448d3debd262e16217a8545ed
Qiqi Wu <1258552199@qq.com>
2023-02-07 20:56:31 +0800

    add event

--- include/gobot/core/events/application_event.hpp
+++ include/gobot/core/events/application_event.hpp
@@ -6,3 +6,65 @@
 */
 
 #pragma once
+
+#include "gobot/core/events/event.hpp"
+
+namespace gobot {
+
+class WindowResizeEvent : public Event {
+    GOBCLASS(WindowResizeEvent, Event)
+public:
+    WindowResizeEvent(unsigned int width, unsigned int height);
+
+    [[nodiscard]] FORCE_INLINE unsigned int GetWidth() const { return width_; }
+
+    [[nodiscard]] FORCE_INLINE unsigned int GetHeight() const { return height_; }
+
+    [[nodiscard]] String ToString() const override;
+
+    EVENT_CLASS_TYPE(WindowResize)
+    EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+private:
+    unsigned int width_;
+    unsigned int height_;
+};
+
+class WindowCloseEvent : public Event {
+    GOBCLASS(WindowCloseEvent, Event)
+public:
+    WindowCloseEvent() = default;
+
+    EVENT_CLASS_TYPE(WindowClose)
+    EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppTickEvent : public Event {
+    GOBCLASS(AppTickEvent, Event)
+public:
+    AppTickEvent() = default;
+
+    EVENT_CLASS_TYPE(AppTick)
+    EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppUpdateEvent : public Event {
+    GOBCLASS(AppUpdateEvent, Event)
+public:
+    AppUpdateEvent() = default;
+
+    EVENT_CLASS_TYPE(AppUpdate)
+    EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+class AppRenderEvent : public Event {
+    GOBCLASS(AppRenderEvent, Event)
+public:
+    AppRenderEvent() = default;
+
+    EVENT_CLASS_TYPE(AppRender)
+    EVENT_CLASS_CATEGORY(EventCategoryApplication)
+};
+
+
+}
