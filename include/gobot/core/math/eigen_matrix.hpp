c1a2c2a878d35d467041312858bf80f11ca13de1
Qiqi Wu <1258552199@qq.com>
2023-02-09 17:43:09 +0800

    update

--- include/gobot/core/math/eigen_matrix.hpp
+++ include/gobot/core/math/eigen_matrix.hpp
@@ -11,24 +11,13 @@
 #include "rttr/type.h"
 
 
-//template<typename Scalar, int _Rows, int _Cols,
-//        int _Options = Eigen::AutoAlign |
-//                       ( (_Rows==1 && _Cols!=1) ? Eigen::RowMajor
-//                                                : (_Cols==1 && _Rows!=1) ? Eigen::ColMajor
-//                                                                         : EIGEN_DEFAULT_MATRIX_STORAGE_ORDER_OPTION ),
-//        int _MaxRows = _Rows,
-//        int _MaxCols = _Cols>
-//class Eigen::Matrix;
-
-
 namespace rttr::detail {
 
 template<typename Scalar, int Rows, int Cols>
 struct template_type_trait<Eigen::Matrix<Scalar, Rows, Cols>> : std::true_type {
     static std::vector<::rttr::type> get_template_arguments() {
-        return {::rttr::type::get<Eigen::Matrix<Scalar, Rows, Cols>>()}; }
+        return {::rttr::type::get<Scalar>(), ::rttr::type::get<int>(), ::rttr::type::get<int>() }; }
 };
-
 
 }
 
