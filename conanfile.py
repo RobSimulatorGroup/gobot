021afa94995b66e89e3709a3e8be816e78bd6bca
Qiqi Wu <1258552199@qq.com>
2023-02-16 16:44:14 +0800

    add qt cache

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class GobotConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package_multi", "cmake_paths"
     def requirements(self):
-        self.requires("opengl/system")
+        # self.requires("opengl/system")
         self.requires("eigen/3.4.0")
         self.requires("spdlog/1.11.0")
         self.requires("nlohmann_json/3.11.2")
@@ -18,8 +18,8 @@ class GobotConan(ConanFile):
         self.requires("sdl/2.26.1")
         self.requires("sdl_image/2.0.5")
 
-    def configure(self):
-        self.options['sdl'].wayland = False
+    # def configure(self):
+    #     self.options['sdl'].wayland = False
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin") # From bin to bin
