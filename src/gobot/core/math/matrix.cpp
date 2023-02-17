ade5d186ee1845f255a6353debdeb192d36d3cfa
Qiqi Wu <1258552199@qq.com>
2023-02-17 15:47:44 +0800

    improve matrix

--- src/gobot/core/math/matrix.cpp
+++ src/gobot/core/math/matrix.cpp
@@ -7,30 +7,64 @@
  */
 
 #include "gobot/core/math/matrix.hpp"
+#include "gobot/core/macros.hpp"
 #include "gobot/core/registration.hpp"
 
+#define GOBOT_MATRIX_MAKE_RTTR_REGISTRATION(Type, TypeSuffix, Size, SizeSuffix)                   \
+    Class_<Matrix##SizeSuffix##TypeSuffix>(GOB_STRINGIFY(Matrix##SizeSuffix##TypeSuffix))         \
+        .constructor()(CtorAsObject)                                                              \
+        .property("storage", &MatrixData<Type>::storage);                                         \
+    Class_<Vector##SizeSuffix##TypeSuffix>(GOB_STRINGIFY(Vector##SizeSuffix##TypeSuffix))         \
+        .constructor()(CtorAsObject)                                                              \
+        .property("storage", &MatrixData<Type>::storage);                                         \
+    Class_<RowVector##SizeSuffix##TypeSuffix>(GOB_STRINGIFY(RowVector##SizeSuffix##TypeSuffix))   \
+        .constructor()(CtorAsObject)                                                              \
+        .property("storage", &MatrixData<Type>::storage);
+
+
+#define GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, Size)                         \
+     Class_<Matrix##Size##X##TypeSuffix>(GOB_STRINGIFY(Matrix##Size##X##TypeSuffix))              \
+        .constructor()(CtorAsObject)                                                              \
+        .property("storage", &MatrixData<Type>::storage);                                         \
+     Class_<Matrix##X##Size##TypeSuffix>(GOB_STRINGIFY(Matrix##X##Size##TypeSuffix))              \
+        .constructor()(CtorAsObject)                                                              \
+        .property("storage", &MatrixData<Type>::storage);                                         \
+
+
+#define GOBOT_MAKE_RTTR_REGISTRATION_ALL_SIZES(Type, TypeSuffix)             \
+GOBOT_MATRIX_MAKE_RTTR_REGISTRATION(Type, TypeSuffix, 2, 2)                  \
+GOBOT_MATRIX_MAKE_RTTR_REGISTRATION(Type, TypeSuffix, 3, 3)                  \
+GOBOT_MATRIX_MAKE_RTTR_REGISTRATION(Type, TypeSuffix, 4, 4)                  \
+GOBOT_MATRIX_MAKE_RTTR_REGISTRATION(Type, TypeSuffix, Eigen::Dynamic, X)     \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 2)               \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 3)               \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 4)               \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 5)               \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 6)               \
+GOBOT_MATRIX_MAKE_FIXED_RTTR_REGISTRATION(Type, TypeSuffix, 7)
+
 GOBOT_REGISTRATION {
-  Class_<MatrixData<int>>("MatrixDatai")
-      .constructor()(CtorAsObject)
-      .property("rows", &MatrixData<int>::rows)
-      .property("cols", &MatrixData<int>::cols)
-      .property("storage", &MatrixData<int>::storage);
+    Class_<MatrixData<int>>("MatrixDatai")
+        .constructor()(CtorAsObject)
+        .property("rows", &MatrixData<int>::rows)
+        .property("cols", &MatrixData<int>::cols)
+        .property("storage", &MatrixData<int>::storage);
 
     Class_<MatrixData<double>>("MatrixDatad")
-            .constructor()(CtorAsObject)
-            .property("rows", &MatrixData<double>::rows)
-            .property("cols", &MatrixData<double>::cols)
-            .property("storage", &MatrixData<double>::storage);
+        .constructor()(CtorAsObject)
+        .property("rows", &MatrixData<double>::rows)
+        .property("cols", &MatrixData<double>::cols)
+        .property("storage", &MatrixData<double>::storage);
 
     Class_<MatrixData<float>>("MatrixDataf")
-            .constructor()(CtorAsObject)
-            .property("rows", &MatrixData<float>::rows)
-            .property("cols", &MatrixData<float>::cols)
-            .property("storage", &MatrixData<float>::storage);
+        .constructor()(CtorAsObject)
+        .property("rows", &MatrixData<float>::rows)
+        .property("cols", &MatrixData<float>::cols)
+        .property("storage", &MatrixData<float>::storage);
 
-  Class_<Matrix3i>("Matrix3i")
-      .constructor()(CtorAsObject)
-      .property("matrix_data", &Matrix3i::GetMatrixData,
-                &Matrix3i::SetMatrixData);
+
+    GOBOT_MAKE_RTTR_REGISTRATION_ALL_SIZES(int,    i)
+    GOBOT_MAKE_RTTR_REGISTRATION_ALL_SIZES(float,  f)
+    GOBOT_MAKE_RTTR_REGISTRATION_ALL_SIZES(double, d)
 
 };
