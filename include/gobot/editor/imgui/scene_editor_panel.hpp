8e035f761bb8ca3550c481706335b7844898330e
Qiqi Wu <1258552199@qq.com>
2023-04-14 17:26:30 +0800

    update

--- include/gobot/editor/imgui/scene_editor_panel.hpp
+++ include/gobot/editor/imgui/scene_editor_panel.hpp
@@ -9,11 +9,27 @@
 
 #include "gobot/scene/imgui_window.hpp"
 
+class ImGuiTextFilter;
+
 namespace gobot {
 
 class GOBOT_EXPORT SceneEditorPanel : public ImGuiWindow {
 public:
     SceneEditorPanel();
+
+    ~SceneEditorPanel() override;
+
+    void OnImGuiContent() override;
+
+    void DrawNode(Node* node);
+
+
+private:
+    ImGuiTextFilter* filter_{nullptr};
+    Node* double_clicked_{nullptr};
+    Node* current_{nullptr};
+    bool select_up_;
+    bool select_down_;
 
 };
 
